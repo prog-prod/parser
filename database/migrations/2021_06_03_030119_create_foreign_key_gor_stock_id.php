4853adfaa84ba999f4198999457c9c4e900f7743
andrew-mintymint <polyvyanyy.andrii@gmail.com>
2021-06-05 13:29:01 +0300

    fix migration

--- database/migrations/2021_06_03_030119_create_foreign_key_gor_stock_id.php
+++ database/migrations/2021_06_03_030119_create_foreign_key_gor_stock_id.php
@@ -13,16 +13,16 @@ class CreateForeignKeyGorStockId extends Migration
      */
     public function up()
     {
-        Schema::table('stock_overviews_history', function (Blueprint $table) {
-            $table->bigInteger('stock_overview_id')->unsigned()->after('id');
-            $table->bigInteger('stock_id')->unsigned()->after('stock_overview_id');
-        });
-        Schema::table('stock_overviews_history', function (Blueprint $table) {
-            $table->foreign('stock_id')->references('id')->on('stocks')->onDelete('cascade');
-        });
-        Schema::table('stock_overviews_history', function (Blueprint $table) {
-            $table->foreign('stock_overview_id')->references('id')->on('stock_overviews')->onDelete('cascade');
-        });
+//        Schema::table('stock_overviews_history', function (Blueprint $table) {
+//            $table->bigInteger('stock_overview_id')->unsigned()->after('id');
+//            $table->bigInteger('stock_id')->unsigned()->after('stock_overview_id');
+//        });
+//        Schema::table('stock_overviews_history', function (Blueprint $table) {
+//            $table->foreign('stock_id')->references('id')->on('stocks')->onDelete('cascade');
+//        });
+//        Schema::table('stock_overviews_history', function (Blueprint $table) {
+//            $table->foreign('stock_overview_id')->references('id')->on('stock_overviews')->onDelete('cascade');
+//        });
     }
 
     /**
@@ -32,10 +32,9 @@ class CreateForeignKeyGorStockId extends Migration
      */
     public function down()
     {
-        Schema::table('stock_overviews_history', function (Blueprint $table) {
-            $table->dropForeign('stock_overviews_history_stock_id_foreign');
-            $table->dropForeign('stock_overviews_history_stock_overview_id_foreign');
-        });
-        Schema::dropColumns('stock_overviews_history',['stock_id','stock_overview_id']);
+//        Schema::table('stock_overviews_history', function (Blueprint $table) {
+//            $table->dropForeign('stock_overviews_history_stock_id_foreign');
+//        });
+//        Schema::dropColumns('stock_overviews_history',['stock_id','stock_overview_id']);
     }
 }
